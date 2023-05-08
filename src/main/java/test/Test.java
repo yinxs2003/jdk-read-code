644391dcdaa80e1c6223e181440f494039f9c492
yinxuesong <yinxuesong@yidian-inc.com>
2023-05-09 07:37:32 +0800

    init code

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,34 @@
+package test;
+
+import javafx.concurrent.Task;
+
+import java.util.concurrent.*;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static java.lang.Thread.sleep;
+
+public class Test {
+    public static void main(String[] args) {
+        ExecutorService executorService = new ThreadPoolExecutor(2, 10,
+                1, TimeUnit.MINUTES, new ArrayBlockingQueue<>(5, true),
+                Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            executorService.execute(() -> {
+                // 获取线程名称,默认格式:pool-1-thread-1
+                System.out.println(System.currentTimeMillis() + " " + Thread.currentThread().getName() + " " + index);
+                // 等待2秒
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+}
